3b8ad078a788ce23482ee09bcaec5fb6d3845c7a
satyakithecoder <rajsatyaki2010@gmail.com>
2024-10-08 15:21:54 +0000

    first commit

---
+++ js/signIn.js
@@ -0,0 +1,20 @@
+const signUp = document.getElementById("sign-up")
+const signIn = document.getElementById("sign-in")
+signUp.addEventListener("click", function(){
+    window.location.href = "./signUp.html" 
+})
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyCLv91NoWJDg14RVehyvMvBWWfYNyCYLxs",
+    authDomain: "quiz-app-d7864.firebaseapp.com",
+    projectId: "quiz-app-d7864",
+    storageBucket: "quiz-app-d7864.appspot.com",
+    messagingSenderId: "835267412301",
+    appId: "1:835267412301:web:8663ad212fa04d537a7704"
+	databaseUrl: "https://quiz-app-d7864-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+
--- js/signUp.js
+++ js/signUp.js
@@ -1,9 +1,6 @@
 const signUp = document.getElementById("sign-up")
-const signIn = document.getElementById("sign-in")
-signUp.addEventListener("click", function(){
-     
-})
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 const firebaseConfig = {
     apiKey: "AIzaSyCLv91NoWJDg14RVehyvMvBWWfYNyCYLxs",
@@ -11,8 +8,28 @@ const firebaseConfig = {
     projectId: "quiz-app-d7864",
     storageBucket: "quiz-app-d7864.appspot.com",
     messagingSenderId: "835267412301",
-    appId: "1:835267412301:web:8663ad212fa04d537a7704"
+    appId: "1:835267412301:web:8663ad212fa04d537a7704",
+	databaseUrl: "https://quiz-app-d7864-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
-
+const db = getDatabase(app)
+const usersinDB = ref(db, "username")
+signUp.addEventListener("click", function(){
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+const username = document.getElementById("username").value	
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+	console.log(user)  
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+	console.log(`${errorCode}: ${errorMessage}`) 
+	alert("An Error Occured while creating your account. Retrty !")  
+  });
+push(usersinDB, username)
+username.value = " "	
+})
