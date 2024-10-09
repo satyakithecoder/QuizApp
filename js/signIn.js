20cc0e290b80a02825ca1ba6647e840eceaea907
satyakithecoder <rajsatyaki2010@gmail.com>
2024-10-09 07:04:40 +0000

    first commit

--- js/signIn.js
+++ js/signIn.js
@@ -16,5 +16,3 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
-
-
--- js/signUp.js
+++ js/signUp.js
@@ -1,36 +1,71 @@
-const signUp = document.getElementById("sign-up")
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+const signUp = document.getElementById("sign-up");
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCLv91NoWJDg14RVehyvMvBWWfYNyCYLxs",
-    authDomain: "quiz-app-d7864.firebaseapp.com",
-    projectId: "quiz-app-d7864",
-    storageBucket: "quiz-app-d7864.appspot.com",
-    messagingSenderId: "835267412301",
-    appId: "1:835267412301:web:8663ad212fa04d537a7704",
-	databaseUrl: "https://quiz-app-d7864-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  apiKey: "AIzaSyCLv91NoWJDg14RVehyvMvBWWfYNyCYLxs",
+  authDomain: "quiz-app-d7864.firebaseapp.com",
+  projectId: "quiz-app-d7864",
+  storageBucket: "quiz-app-d7864.appspot.com",
+  messagingSenderId: "835267412301",
+  appId: "1:835267412301:web:8663ad212fa04d537a7704",
 };
+
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app)
-const auth = getAuth(app)
-const usersinDB = ref(db, "username")
-signUp.addEventListener("click", function(e){
-e.preventDefault()	
-const email = document.getElementById("email").value
-const password = document.getElementById("password").value
-const username = document.getElementById("username").value	
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
-	console.log(user)  
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-	console.log(`${errorCode}: ${errorMessage}`) 
-	alert("An Error Occured while creating your account. Retrty !")  
-  });
-push(usersinDB, username)
-username.value = " "	
-})
+const auth = getAuth(app);
+const db = getFirestore(app);
+signUp.addEventListener("click", async function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
+
+
+  const res = await emailValidate(email);
+  if (res === false) {
+    console.error("Invalid email");
+    alert("Not a valid email");
+    return;
+  }
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(async (userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+
+      try {
+        const docRef = await addDoc(collection(db, "users"), {
+          username: username,
+          email: email
+        });
+        console.log("User added to Firestore with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document to Firestore: ", e);
+      }
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(`${errorCode}: ${errorMessage}`);
+      alert("An Error Occurred while creating your account. Please retry!");
+    });
+});
+
+async function emailValidate(user) {
+  try {
+    const response = await fetch(`https://emailvalidation.abstractapi.com/v1/?api_key=b8b945ae1a4842228155a5216491fd3c&email=${user}`);
+
+    if (!response.ok) return false;
+    const data = await response.json();
+
+    console.log(data);
+    return data.deliverability === "DELIVERABLE";
+
+  } catch (error) {
+    console.error("Email validation failed", error);
+    return false;
+  }
+}
