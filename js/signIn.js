122226850eae5bc938b1371cbbbae188659cfaf4
satyakithecoder <rajsatyaki2010@gmail.com>
2024-10-09 13:17:55 +0000

    first commit

--- js/signIn.js
+++ js/signIn.js
@@ -1,18 +1,64 @@
-const signUp = document.getElementById("sign-up")
-const signIn = document.getElementById("sign-in")
-signUp.addEventListener("click", function(){
-    window.location.href = "./signUp.html" 
-})
+const signUp = document.getElementById("sign-up");
+const signIn = document.getElementById("sign-in");
+const googleSignInbtn = document.getElementById("google-sign-in");
+signUp.addEventListener("click", function () {
+  window.location.href = "./signUp.html";
+});
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyCLv91NoWJDg14RVehyvMvBWWfYNyCYLxs",
-    authDomain: "quiz-app-d7864.firebaseapp.com",
-    projectId: "quiz-app-d7864",
-    storageBucket: "quiz-app-d7864.appspot.com",
-    messagingSenderId: "835267412301",
-    appId: "1:835267412301:web:8663ad212fa04d537a7704",
-	databaseUrl: "https://quiz-app-d7864-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  apiKey: "AIzaSyCLv91NoWJDg14RVehyvMvBWWfYNyCYLxs",
+  authDomain: "quiz-app-d7864.firebaseapp.com",
+  projectId: "quiz-app-d7864",
+  storageBucket: "quiz-app-d7864.appspot.com",
+  messagingSenderId: "835267412301",
+  appId: "1:835267412301:web:8663ad212fa04d537a7704",
+  databaseUrl:
+    "https://quiz-app-d7864-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+signIn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+      console.log(user);
+      window.location.href = "./quiz.html";
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert("SignIn Failed");
+      console.error(`${errorCode}: ${errorMessage}`);
+    });
+  document.getElementById("email").value = "";
+});
+googleSignInbtn.addEventListener("click", function () {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log(token);
+      console.log("User SignedIn. User Info:" + user);
+      window.location.href = "./quiz.html"
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log(email, credential);
+      console.error(`${errorCode}: ${errorMessage}`);
+      alert("Google Sign-In Failed");
+    });
+});
--- js/signUp.js
+++ js/signUp.js
@@ -1,6 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 const signUp = document.getElementById("sign-up");
 
@@ -32,7 +39,6 @@ signUp.addEventListener("click", async function (e) {
     console.error("Invalid email");
     alert("Not a valid email");
     return;
-    
   }
 
   createUserWithEmailAndPassword(auth, email, password)
@@ -44,7 +50,7 @@ signUp.addEventListener("click", async function (e) {
         const docRef = await addDoc(collection(db, "users"), {
           username: username,
           email: email,
-          password: password
+          password: password,
         });
         console.log("User added to Firestore with ID: ", docRef.id);
       } catch (e) {
@@ -58,26 +64,27 @@ signUp.addEventListener("click", async function (e) {
       alert("An Error Occurred while creating your account. Please retry!");
     });
 
-    emailInput.value = "";
-    passwordInput.value = "";
-    usernameInput.value = ""; 
-     
-   setTimeout(() => {
-      alert("You will be redirected to signIn page in few seconds")
-      window.location.href = "./index.html"
-   }, 8000);
+  emailInput.value = "";
+  passwordInput.value = "";
+  usernameInput.value = "";
+
+  setTimeout(() => {
+    alert("You will be redirected to signIn page in few seconds");
+    window.location.href = "./index.html";
+  }, 8000);
 });
 
 async function emailValidate(user) {
   try {
-    const response = await fetch(`https://emailvalidation.abstractapi.com/v1/?api_key=b8b945ae1a4842228155a5216491fd3c&email=${user}`);
+    const response = await fetch(
+      `https://emailvalidation.abstractapi.com/v1/?api_key=b8b945ae1a4842228155a5216491fd3c&email=${user}`
+    );
 
     if (!response.ok) return false;
     const data = await response.json();
 
     console.log(data);
     return data.deliverability === "DELIVERABLE";
-
   } catch (error) {
     console.error("Email validation failed", error);
     return false;
